d9e183ab00c6b28039030f6e6ddd67463eaa1ca5
Peter Banh <peterbanhe.e@gmail.com>
2024-04-05 14:10:40 -0500

    Server.js placeholder

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("Hello World");
+});
+
+app.listen(8000, () => {
+    console.log("This app is listening to port 8000");
+})
