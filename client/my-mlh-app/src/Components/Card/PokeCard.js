373cbff1ad32ead8dd84b208c71a308f641fbea9
Peter Banh <peterbanhe.e@gmail.com>
2024-04-05 15:57:51 -0500

    Added placeholder components

---
+++ client/my-mlh-app/src/Components/Card/PokeCard.js
@@ -0,0 +1,10 @@
+function PokeCard() {
+    return (
+        <div className="pokemon-card">
+            <h1 className="poke-name">Pokemon Name</h1>
+            <p>Typing here</p>
+        </div>
+    )
+}
+
+export default PokeCard
---
+++ client/my-mlh-app/src/Components/Search.js
@@ -0,0 +1,10 @@
+function Search() {
+    return (
+        <div className="search-container">
+            <input type="text" className="search-bar" value=""></input>
+            <button className="search-bttn">Submit</button>
+        </div>
+    )
+}
+
+export default Search;
